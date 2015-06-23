7fa69ee10a970b1cb03df2405f9167036cd53cd7
Etienne Dauvergne <contact@ekyna.com>
2015-06-23 15:20:45 +0200

    Fix fixtures

--- DataFixtures/ORM/LoadEventData.php
+++ DataFixtures/ORM/LoadEventData.php
@@ -6,7 +6,6 @@ use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use Ekyna\Bundle\AgendaBundle\Entity\Event;
 use Faker\Factory;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -37,19 +36,21 @@ class LoadEventData extends AbstractFixture implements FixtureInterface, Ordered
     public function load(ObjectManager $om)
     {
         $faker = Factory::create($this->container->getParameter('hautelook_alice.locale'));
+        $repo = $this->container->get('ekyna_agenda.event.repository');
 
         for ($e = 1; $e < 100; $e++) {
             $startDate = $faker->dateTimeBetween('-3 months', 'now');
             $endDate = clone $startDate;
             $endDate->modify(sprintf('+%s hours', rand(1, 8)));
 
-            $event = new Event();
+            /** @var \Ekyna\Bundle\AgendaBundle\Model\EventInterface $event */
+            $event = $repo->createNew();
             $event
                 ->setName(sprintf('Agenda event %d name', $e))
-                ->setTitle(sprintf('Agenda event %d title', $e))
                 ->setStartDate($startDate)
                 ->setEndDate($endDate)
                 ->setEnabled(true)
+                ->setTitle(sprintf('Agenda event %d title', $e))
                 ->setContent('<p>' . $faker->paragraph(rand(4, 6)) . '</p>')
             ;
 
